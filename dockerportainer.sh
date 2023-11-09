d740dd79efb9a7542f7669fd12cec7005ef4d725
toxikmicrowave <67723896+toxikmicrowave@users.noreply.github.com>
2023-11-09 02:42:15 +0000

    Update dockerportainer.sh

--- dockerportainer.sh
+++ dockerportainer.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Function to print a message in a box
+print_in_box() {
+  local message="$1"
+  local edge="#######################################"
+  local padding="#                                     #"
+
+  echo "$edge"
+  echo "$padding"
+  echo "# $message"
+  echo "$padding"
+  echo "$edge"
+}
+
 # Update the system
+print_in_box "Updating the system"
 sudo apt update
 sudo apt upgrade -y
 
 # Install Docker + Docker Compose
+print_in_box "Installing Docker, Docker Compose + Portainer"
 sudo apt install -y docker.io
 sudo apt install -y docker-compose
 
@@ -16,4 +31,4 @@ sudo systemctl enable docker
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
-echo "Docker, Docker Compose and Portainer installed."
+print_in_box "Complete!"
